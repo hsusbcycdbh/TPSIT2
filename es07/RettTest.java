52eaba889b6887e6eae64478cf5e6af98d5f9262
hsusbcycdbh <153681096+hsusbcycdbh@users.noreply.github.com>
2025-01-10 08:14:37 +0000

    dfddd

---
+++ es07/RettTest.java
@@ -0,0 +1,17 @@
+public class RettTest{
+    
+}
+class rettangolo {
+      int base;
+       int altezza;
+
+    public int Area(){
+        return base * altezza;
+    }
+}
+public class rettangoloMain{
+
+    public class void main (String[]args){
+
+    }
+}
