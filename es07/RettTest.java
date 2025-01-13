2324af515b80f92ad9c2e68dae46f4204fc4912e
hsusbcycdbh <153681096+hsusbcycdbh@users.noreply.github.com>
2025-01-13 12:03:23 +0000

    gggfgf

--- es07/RettTest.java
+++ es07/RettTest.java
@@ -1,17 +1,46 @@
-public class RettTest{
-    
-}
-class rettangolo {
-      int base;
-       int altezza;
+public class Rettangolo {
+    private int base;
+    private int altezza;
 
-    public int Area(){
+    public Rettangolo() {
+        this.base = 1;
+        this.altezza = 1;
+    }
+
+    public int getbase() {
+        return base;
+    }
+
+    public void setbase(int base) {
+        if (base > 0) {
+            this.base = base;
+        } else {
+            this.base = 1;
+            System.out.println("Errore: base non valida. Impostato a 1.");
+        }
+    }
+
+    public int getAltezza() {
+        return altezza;
+    }
+
+    public void setAltezza(int altezza) {
+        if (altezza > 0) {
+            this.altezza = altezza;
+        } else {
+            this.altezza = 1;
+            System.out.println("Errore: altezza non valida. Impostato a 1.");
+        }
+    }
+
+    public int calcolaArea() {
         return base * altezza;
     }
-}
-public class rettangoloMain{
 
-    public class void main (String[]args){
-
+    public static void main(String[] args) {
+        Rettangolo rettangolo = new Rettangolo();
+        rettangolo.setbase(-5);
+        rettangolo.setAltezza(-3);
+        System.out.println("Area del rettangolo: " + rettangolo.calcolaArea());
     }
 }
